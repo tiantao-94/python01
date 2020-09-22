cded7ff5eca8be8ad77c9788e7985c53b9924e93
tiaotao <1595328432@qq.com>
2020-09-22 20:17:27 +0800

    试图和url练习已完成

--- bookmanger/book/views.py
+++ bookmanger/book/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+# 导入httpresponse模块
+from django.http import HttpResponse
+
+
+# 定义视图函数
+def index(request):
+    return HttpResponse('ok!')
